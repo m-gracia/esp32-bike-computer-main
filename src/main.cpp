ec1e3b1b3963c1f7e93ef9ad2074a908dc29b528
m-gracia <manuel@manuelgracia.me>
2023-05-01 13:22:08 +0200

    Disabled Brownout detector

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include "lcd.h"
 #include "gprs.h"
 #include "tpms.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+//#include "soc/rtc_wdt.h"
 
 
 // TASK ON CORE 0 //
@@ -22,12 +25,18 @@ void loopOthers (void * parameter){
 
 
 void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // Disable browout detector
+  //rtc_wdt_protect_off(); // Disable watchdog
+  //rtc_wdt_disable();
+  //disableCore1WDT();
+  //disableCore0WDT();
+
   #ifdef DEBUG
     Serial.begin(115200);
   #endif
-  DEBUG_PRINTLN("Manuel Gracia.Ene-2023");
+  DEBUG_PRINTLN("Manuel Gracia.Abr-2023");
   DEBUG_PRINTLN("https://github.com/m-gracia");
-  DEBUG_PRINTLN("esp32-bike-computer_20230129");
+  DEBUG_PRINTLN("esp32-bike-computer_20230430");
 
   pinMode(LED_PIN,OUTPUT);
   for(int i=0;i<4; i++){  // Wait until the current is stabilized
@@ -78,7 +87,7 @@ void loop() {
     getTPMS();
     timerTPMS = millis() + (BTSCANTIME * 1000) + 200; // 5 sec
   }
-  
+
   if (timerGPS < millis() && bikeGPS != STATUS_UNK){
     getGPS(); // Every time
     timerGPS = millis() + 200;  // 200ms
